60b60bacf8daa899aef3bba6e358fad4e7251b0e
Hiich <56564585+Hiich@users.noreply.github.com>
2021-10-11 22:14:52 +0200

    Update preview.tsx

--- pages/preview.tsx
+++ pages/preview.tsx
@@ -5,12 +5,14 @@ import Image from 'next/image'
 import { NextSeo as SEO } from 'next-seo'
 import React from 'react'
 import config from '@/modules/config'
+import { useRouter } from 'next/router'
 
 const desktopLinks = [
   { name: 'Collection preview', href: '/preview' },
 ]
 
 const IndexPage: NextPage = () => {
+  const router = useRouter()
   const { gallery, otherPageLinks } = config
 
   return (
@@ -39,13 +41,26 @@ const IndexPage: NextPage = () => {
           <div className="relative h-24 mx-auto">
             <Image src="/yubba-world-1line.png" layout="fill" objectFit="contain" alt="Yubba World" />
           </div>
-
-          {/* title */}
-          <H1 className="text-secondary" style={{ marginTop: '40px', marginBottom: '48px'  }}>
-            Collection Preview
-          </H1>
-          
         </Container>
+                
+        <div className="flex flex-row items-center w-full px-4 mx-auto max-w-7xl">
+          <div className="hidden w-24 md:flex">
+            <Button variant="white" style={{ fontWeight: 400, border: '1px solid #cfcfcf', justifyContent: "center" }} block onClick={() => router.back()}>
+              <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mr-2 fill-current" fill="currentColor" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+              </svg>
+              Back
+            </Button>
+          </div>
+
+          <div className="flex justify-center flex-1 flex-grow">
+            <H1 className="text-center text-secondary" style={{ marginTop: '40px', marginBottom: '40px' }}>
+              Collection Preview
+            </H1>
+          </div>
+
+          <div className="hidden w-32 md:flex"></div>
+        </div>  
 
         {/* the collection preview */}
         {/* onside of Container to display 2 columns on iPhone X */}
