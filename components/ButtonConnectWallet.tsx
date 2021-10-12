0df1aa1b3d8eca5a717325b15cb09080257ae333
Hiich <56564585+Hiich@users.noreply.github.com>
2021-10-12 11:53:07 +0200

    Update ButtonConnectWallet.tsx

--- components/ButtonConnectWallet.tsx
+++ components/ButtonConnectWallet.tsx
@@ -3,7 +3,7 @@ import { Button } from ".";
 import { ButtonProps } from "./Button/Button";
 
 export const ButtonConnectWallet: FC<ButtonProps> = ({ variant, ...otherProps }) => (
-  <Button variant="primary"  {...otherProps}  style={{ justifyContent: 'center' }} >
+  <Button variant="primary"  {...otherProps}  style={{ justifyContent: 'center' , fontWeight: "lighter",fontSize:"12px", color:"#fffeff", height:"27px"}} >
     Connect wallet
   </Button>
 )
