211584d32fa227f9bba1cac20a64a8525df08b73
Hiich <56564585+Hiich@users.noreply.github.com>
2021-10-13 11:07:09 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,33 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { DefaultSeo } from 'next-seo'
-import config from '@/modules/config'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { DefaultSeo } from "next-seo";
+import config from "@/modules/config";
+import { Provider } from "react-redux";
+import store from "../redux/store";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <DefaultSeo
-        defaultTitle={`Yubba World™️ Official Site - NFT Collectibles`}
-        titleTemplate={`%s - Yubba World Official Site - NFT Collectibles`}
-        description="Yubba World is a collection of randomly generated digital collectibles of various rarities living eternally on the Ethereum blockchain. Join our community now to learn more and be kept up-to-date on all things Yubba." 
+        defaultTitle={config.app.name}
+        titleTemplate={`${config.app.name} - %s`}
         openGraph={{
-          type: 'website',
-          locale: 'en_EN',
+          type: "website",
+          locale: "en_EN",
           url: config.app.url,
-          site_name: `Yubba World`,
+          site_name: config.app.name,
         }}
         additionalLinkTags={[
           {
-            rel: 'icon',
-            href: `/favicon.png`,
+            rel: "icon",
+            href: `${config.app.url}/favicon.png`,
           },
         ]}
       />
-      <Component {...pageProps} />
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
     </>
-  )
+  );
 }
-export default MyApp
+export default MyApp;
