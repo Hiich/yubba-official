895858c83e90f001a6315bf4b9dfe6e7e9f6428a
Hiich <56564585+Hiich@users.noreply.github.com>
2021-11-19 08:13:22 +0100

    Add files via upload

--- components/Presale.tsx
+++ components/Presale.tsx
@@ -10,42 +10,46 @@ interface Props {
 }
 
 export default function Presale({ className }: Props) {
-  const discord_link = "https://discord.com/invite/yubbaworld";
+  const discord_link = 'https://discord.com/invite/yubbaworld'
 
   return (
-    <div className={clsx("flex-col items-center", className)}>
+    <div className={clsx('flex-col items-center', className)}>
       <div className="bg-[#09b8c3] rounded-tl-[4px] rounded-tr-[4px] relative text-white w-full justify-start px-6 h-16 flex items-center font-bold text-lg tracking-wide">
-        <strong>
-          Coming soon
-        </strong>
+        <strong>Public Launch</strong>
         <div className="absolute -top-9 right-5">
           <Image src="/icon-stars.png" width="75" height="75" alt="Stars" />
         </div>
       </div>
       <div className="bg-gradient-to-b from-[#ffffff] to-[#ebebeb] rounded-bl-[4px] rounded-br-[4px] w-full justify-start px-6 pt-4 pb-10">
-        <H2 className="text-secondary font-title" style={{ marginBottom: "0", fontWeight: 900 }}>
-          Pre-sale
+        <H2
+          className="text-secondary font-title"
+          style={{ marginBottom: '0', fontWeight: 900 }}
+        >
+          Public Launch
         </H2>
-        
+
         <p className="mt-2">
-          Registration now open
+          Pre-sale : November 20th
+          <br />
+          <br /> Public launch : November 21st
+          <br />
+          <br />
+          Mint price : 0.07
+          <br />
         </p>
-        
-        <div className="flex flex-row space-x-4 my-4">
-          <Image src="/badges/icon-coolcats-badge.png" width="45" height="45" layout="fixed" alt="Badge" />
-          <Image src="/badges/icon-deadfellaz-badge.png" width="45" height="45" layout="fixed" alt="Badge" />
-          <Image src="/badges/icon-pudgypenguins-badge.png" width="45" height="45" layout="fixed" alt="Badge" />
-        </div>
-        
-        <p>
-          Honorary <strong>Pre-sale whitelist</strong> available for Cool Cats, Pudgy Penguins and Deadfellaz holders.
-        </p>
-        
+
         <div className="mt-7">
           <a href={discord_link} target="_blank" rel="noreferrer">
-            <Button variant="primary" size="large" style={{ fontWeight: 700, paddingLeft: '24px', paddingRight: '24px' }}>
-              Register on Discord
-              <DiscordIcon className="ml-2" />
+            <Button
+              variant="primary"
+              size="large"
+              style={{
+                fontWeight: 700,
+                paddingLeft: '24px',
+                paddingRight: '24px',
+              }}
+            >
+              Minting on November 20th
             </Button>
           </a>
         </div>
--- components/Team.tsx
+++ components/Team.tsx
@@ -6,7 +6,6 @@ export const Team = () => {
   const { founders, investors } = config
 
   const founder1 = founders[0]
- 
 
   const investor1 = investors[0]
   const investor2 = investors[1]
@@ -16,30 +15,27 @@ export const Team = () => {
   const investor6 = investors[5]
 
   return (
-    <section className="pt-20 my-16" id="#team">      
+    <section className="pt-20 my-16" id="#team">
       <Container>
-
         <H1 className="mb-5 text-center text-secondary">TEAM</H1>
 
         <div className="bg-[#FFFFFF] bg-opacity-50 rounded border border-[#cfcfcf] px-12 py-5 pb-10 text-center">
-          
           <H2 className="mt-2 mb-8 text-secondary">Founders</H2>
-          
-          <div className="grid grid-cols-1 gap-10 md:grid-cols-4">
+
+          <div>
             <Teammate {...founder1} className="md:col-start-2" />
           </div>
 
           <H2 className="mt-16 mb-8 text-secondary">Investors & Advisors</H2>
-          
+
           <div className="grid grid-cols-2 gap-10 md:grid-cols-4">
             <Teammate {...investor1} />
             <Teammate {...investor2} />
             <Teammate {...investor3} />
             <Teammate {...investor4} />
             <Teammate {...investor5} className="md:col-start-2" />
-            <Teammate {...investor6} />            
+            <Teammate {...investor6} />
           </div>
-
         </div>
       </Container>
     </section>
